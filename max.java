51e83c32766cf803f3d5c3f9240bbdd608d5f129
AndrewUrus117 <93611789+Krane117@users.noreply.github.com>
2024-07-10 20:32:28 +0200

    Add files via upload

---
+++ max.java
@@ -0,0 +1,15 @@
+package org.example;
+
+class Solution {
+    public int minPartitions(String n) {
+        int len = n.length ();
+        char max = '0';
+        for (int i = 0; i < len; i++) {
+            char symbol = n.charAt(i);
+            if (symbol > max) {
+                max = symbol;
+            }
+        }
+            return max - '0';
+      }
+    }
